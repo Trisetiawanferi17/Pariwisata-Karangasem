19b520b7c30f8d21f6bfd2fde0ac541b6a325a4a
oktapioni02 <meetsunrise.2@gmail.com>
2026-04-20 23:24:54 +0800

    update social media links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -536,10 +536,10 @@
                     <a href="https://www.facebook.com/share/1CrMC2mAuz/?mibextid=wwXIfr" class="social-icon" target="_blank" rel="noopener noreferrer">
                         <i class="fab fa-facebook-f"></i>
                     </a>
-                    <a href="https://www.instagram.com/pesonakarangasem" class="social-icon" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.instagram.com/pesona_karangasem01?igsh=aGF2ZXBkbmJoaHNx&utm_source=qr" class="social-icon" target="_blank" rel="noopener noreferrer">
                         <i class="fab fa-instagram"></i>
                     </a>
-                    <a href="https://www.youtube.com/@pesonakarangasem" class="social-icon" target="_blank" rel="noopener noreferrer">
+                    <a href="https://youtube.com/@pesonakarangasem?si=tvNpTYx9eFbR1Xcn" class="social-icon" target="_blank" rel="noopener noreferrer">
                         <i class="fab fa-youtube"></i>
                     </a>
                 </div>
